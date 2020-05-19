8a1dc6dfdf500cbd814aaf3c679afecc6ac5a6ca
Flockshot <m.flockshot@gmail.com>
2020-05-19 08:56:21 +0500

    1.0

--- src/me/flockshot/factionupgrades/utils/filesystem/UnknownConfig.java
+++ src/me/flockshot/factionupgrades/utils/filesystem/UnknownConfig.java
@@ -9,55 +9,55 @@ import me.flockshot.factionupgrades.utils.files.identifier.FileIdentifier;
 
 
 public abstract class UnknownConfig implements FlockConfig
-{	
-	private FileConfiguration config;	
-	
-	public UnknownConfig(FileConfiguration config)
-	{
-		setConfig(config);
-		
-		setDefaults();
-		getConfig().options().copyDefaults(true);
-		save();
-	}
-	
-	
-	@Override
-	public Object getLine(FileIdentifier identifier) {
-		Object value = getConfig().get(identifier.toString());
-		if(value instanceof String)
-			return ColorTranslator.getString((String) value);
-		else
-			return value;		 
-	}
-	@Override
-	public void setLine(FileIdentifier identifier, Object value) {
-		getConfig().set(identifier.toString(), value);
-		save();
-	}
-	@Override
-	public void setLine(String identifier, Object value) {
-		getConfig().set(identifier, value);
-	}
-	
-	@Override
-	public void addDefault(FileIdentifier path, Object value) {
-		if(value instanceof List)
-			getConfig().addDefault(path.toString(), (List<?>) value);
-		else if(value instanceof String)
-			getConfig().addDefault(path.toString(), (String) value);
-		else
-			getConfig().addDefault(path.toString(), value);		
-	}
+{    
+    private FileConfiguration config;    
+    
+    public UnknownConfig(FileConfiguration config)
+    {
+        setConfig(config);
+        
+        setDefaults();
+        getConfig().options().copyDefaults(true);
+        save();
+    }
+    
+    
+    @Override
+    public Object getLine(FileIdentifier identifier) {
+        Object value = getConfig().get(identifier.toString());
+        if(value instanceof String)
+            return ColorTranslator.getString((String) value);
+        else
+            return value;         
+    }
+    @Override
+    public void setLine(FileIdentifier identifier, Object value) {
+        getConfig().set(identifier.toString(), value);
+        save();
+    }
+    @Override
+    public void setLine(String identifier, Object value) {
+        getConfig().set(identifier, value);
+    }
+    
+    @Override
+    public void addDefault(FileIdentifier path, Object value) {
+        if(value instanceof List)
+            getConfig().addDefault(path.toString(), (List<?>) value);
+        else if(value instanceof String)
+            getConfig().addDefault(path.toString(), (String) value);
+        else
+            getConfig().addDefault(path.toString(), value);        
+    }
 
-	@Override
-	public FileConfiguration getConfig() {
-		return config;
-	}
-	@Override
-	public void setConfig(FileConfiguration config) {
-		this.config = config;
-	}
-	
+    @Override
+    public FileConfiguration getConfig() {
+        return config;
+    }
+    @Override
+    public void setConfig(FileConfiguration config) {
+        this.config = config;
+    }
+    
 
 }
